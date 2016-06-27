01f5e7b5c6dbda17f7be76e9074be85e982e7259
Meghan Hebel <mhebel@asu.edu>
2016-06-26 19:05:48 -0700

    Added files for superCool

--- hideBlueHideRed/ViewController.swift
+++ hideBlueHideRed/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var redBomb: UIImageView!
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideRedBomb(sender: AnyObject) {
+        blueBomb.hidden = false
+        redBomb.hidden = true
+    }
 
+    @IBAction func hideBlueBomb(sender: AnyObject) {
+        blueBomb.hidden = true
+        redBomb.hidden = false
+    }
+    
+    @IBAction func resetImages(sender: AnyObject) {
+        blueBomb.hidden = false
+        redBomb.hidden = false
+    }
+    
 }
 
